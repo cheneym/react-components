2a2fdede31fb9dc78a86f37e5430ed01252deb98
Cheney Mao <cheneymao94@gmail.com>
2016-09-30 11:15:24 -0700

    Refactor GroceryListItem to be a class component

--- app.js
+++ app.js
@@ -1,37 +1,21 @@
-var GroceryList = (cart) => (
-  <div>
-    <GroceryListItem grocery = {cart.groceries[0]} />
-    <GroceryListItem grocery = {cart.groceries[1]} />
-    <GroceryListItem grocery = {cart.groceries[2]} />
-    <GroceryListItem grocery = {cart.groceries[3]} />
-  </div>
-)
+class GroceryListItem extends React.Component {
 
-// var Cucumbers = () => (
-//   <div> yolo </div>
-// )
+  constructor(props) {
+    super(props);
+  }
 
-// var Kale = () => (
-//   <div> pce </div>
-// )
+  render() {
+    return (
+      <li>{this.props.grocery}</li>
+    );
+  }
+}
 
-var GroceryListItem = (item) => (
-  <div> {item.grocery} </div>
-)
-
-var TodoList = (props) => (
+var GroceryList = (props) => (
   <ul>
-    <li>{props.todos[0]}</li>
-    <li>{props.todos[1]}</li>
-    <li>{props.todos[2]}</li>
+    {props.groceries.map(grocery =>
+      <GroceryListItem grocery={grocery} />
+    )}
   </ul>
 );
-
-var App = () => (
-  <div>
-    <h2>My Todo List</h2>
-    <GroceryList groceries={['Learn React', 'Crush Recast.ly', 'Maybe sleep']}/> // Here we are creating an instance of the `TodoList` component
-  </div>
-);
 ReactDOM.render(<GroceryList groceries={['Apples', 'and', 'Oranges']}/>, document.getElementById("app"));
-
